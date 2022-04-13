8c3b309e84184d5030255838746aa08d473ff657
Landon <33662627+landonviator@users.noreply.github.com>
2022-04-13 16:29:41 -0500

    re-positioned faders for graphic eq

--- Multi-Q/Source/UI-Components/GraphicEQComponent.cpp
+++ Multi-Q/Source/UI-Components/GraphicEQComponent.cpp
@@ -39,18 +39,21 @@ void GraphicEQComponent::paint (juce::Graphics& g)
 void GraphicEQComponent::resized()
 {
     auto leftMargin = getWidth() * 0.1;
-    auto topMargin = getHeight() * 0.15;
-    auto faderheight = getHeight() * 0.55;
-    auto faderWidth = faderheight * 0.25;
+    auto topMargin = getHeight() * 0.12;
+    auto faderheight = getHeight() * 0.28;
+    auto faderWidth = faderheight * 0.65;
 
     band1.setBounds(leftMargin, topMargin, faderWidth, faderheight);
     band2.setBounds(band1.getX() + band1.getWidth(), topMargin, faderWidth, faderheight);
     band3.setBounds(band2.getX() + band2.getWidth(), topMargin, faderWidth, faderheight);
     band4.setBounds(band3.getX() + band3.getWidth(), topMargin, faderWidth, faderheight);
     band5.setBounds(band4.getX() + band4.getWidth(), topMargin, faderWidth, faderheight);
-    band6.setBounds(band5.getX() + band5.getWidth(), topMargin, faderWidth, faderheight);
-    band7.setBounds(band6.getX() + band6.getWidth(), topMargin, faderWidth, faderheight);
-    band8.setBounds(band7.getX() + band7.getWidth(), topMargin, faderWidth, faderheight);
-    band9.setBounds(band8.getX() + band8.getWidth(), topMargin, faderWidth, faderheight);
-    band10.setBounds(band9.getX() + band9.getWidth(), topMargin, faderWidth, faderheight);
+    
+    auto bottomMargin = band4.getY() + band4.getHeight() * 1.25;
+    
+    band6.setBounds(band1.getX(), bottomMargin, faderWidth, faderheight);
+    band7.setBounds(band2.getX(), bottomMargin, faderWidth, faderheight);
+    band8.setBounds(band3.getX(), bottomMargin, faderWidth, faderheight);
+    band9.setBounds(band4.getX(), bottomMargin, faderWidth, faderheight);
+    band10.setBounds(band5.getX(), bottomMargin, faderWidth, faderheight);
 }
