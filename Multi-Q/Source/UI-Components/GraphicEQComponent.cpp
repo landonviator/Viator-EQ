777b388483bf45cc1055a5e71b6f334964dd2bf8
Landon <33662627+landonviator@users.noreply.github.com>
2022-04-17 18:29:35 -0500

    added resizing labels to graphic eq component

--- Multi-Q/Source/UI-Components/GraphicEQComponent.cpp
+++ Multi-Q/Source/UI-Components/GraphicEQComponent.cpp
@@ -15,15 +15,65 @@
 GraphicEQComponent::GraphicEQComponent()
 {
     addAndMakeVisible(band1);
+    addAndMakeVisible(band1Label);
+    band1Label.setText("31", juce::dontSendNotification);
+    band1Label.attachToComponent(&band1, false);
+    band1Label.setJustificationType(juce::Justification::centred);
+    
     addAndMakeVisible(band2);
+    addAndMakeVisible(band2Label);
+    band2Label.setText("63", juce::dontSendNotification);
+    band2Label.attachToComponent(&band2, false);
+    band2Label.setJustificationType(juce::Justification::centred);
+    
     addAndMakeVisible(band3);
+    addAndMakeVisible(band3Label);
+    band3Label.setText("125", juce::dontSendNotification);
+    band3Label.attachToComponent(&band3, false);
+    band3Label.setJustificationType(juce::Justification::centred);
+    
     addAndMakeVisible(band4);
+    addAndMakeVisible(band4Label);
+    band4Label.setText("250", juce::dontSendNotification);
+    band4Label.attachToComponent(&band4, false);
+    band4Label.setJustificationType(juce::Justification::centred);
+    
     addAndMakeVisible(band5);
+    addAndMakeVisible(band5Label);
+    band5Label.setText("500", juce::dontSendNotification);
+    band5Label.attachToComponent(&band5, false);
+    band5Label.setJustificationType(juce::Justification::centred);
+    
     addAndMakeVisible(band6);
+    addAndMakeVisible(band6Label);
+    band6Label.setText("1K", juce::dontSendNotification);
+    band6Label.attachToComponent(&band6, false);
+    band6Label.setJustificationType(juce::Justification::centred);
+    
     addAndMakeVisible(band7);
+    addAndMakeVisible(band7Label);
+    band7Label.setText("2K", juce::dontSendNotification);
+    band7Label.attachToComponent(&band7, false);
+    band7Label.setJustificationType(juce::Justification::centred);
+    
     addAndMakeVisible(band8);
+    addAndMakeVisible(band8Label);
+    band8Label.setText("4K", juce::dontSendNotification);
+    band8Label.attachToComponent(&band8, false);
+    band8Label.setJustificationType(juce::Justification::centred);
+    
     addAndMakeVisible(band9);
+    addAndMakeVisible(band9Label);
+    band9Label.setText("8K", juce::dontSendNotification);
+    band9Label.attachToComponent(&band9, false);
+    band9Label.setJustificationType(juce::Justification::centred);
+    
     addAndMakeVisible(band10);
+    addAndMakeVisible(band10Label);
+    band10Label.setText("15K", juce::dontSendNotification);
+    band10Label.attachToComponent(&band10, false);
+    band10Label.setJustificationType(juce::Justification::centred);
+    
     
 }
 
@@ -45,16 +95,34 @@ void GraphicEQComponent::resized()
     auto spaceBetween = 1.85;
 
     band1.setBounds(leftMargin, topMargin, faderWidth, faderheight);
+    band1Label.setFont(juce::Font ("Helvetica", band1.getWidth() * 0.2f, juce::Font::FontStyleFlags::bold));
+
     band2.setBounds(band1.getX() + band1.getWidth() * spaceBetween, topMargin, faderWidth, faderheight);
+    band2Label.setFont(juce::Font ("Helvetica", band1.getWidth() * 0.2f, juce::Font::FontStyleFlags::bold));
+    
     band3.setBounds(band2.getX() + band2.getWidth() * spaceBetween, topMargin, faderWidth, faderheight);
+    band3Label.setFont(juce::Font ("Helvetica", band1.getWidth() * 0.2f, juce::Font::FontStyleFlags::bold));
+    
     band4.setBounds(band3.getX() + band3.getWidth() * spaceBetween, topMargin, faderWidth, faderheight);
+    band4Label.setFont(juce::Font ("Helvetica", band1.getWidth() * 0.2f, juce::Font::FontStyleFlags::bold));
+    
     band5.setBounds(band4.getX() + band4.getWidth() * spaceBetween, topMargin, faderWidth, faderheight);
+    band5Label.setFont(juce::Font ("Helvetica", band1.getWidth() * 0.2f, juce::Font::FontStyleFlags::bold));
     
     auto bottomMargin = band4.getY() + band4.getHeight() * 1.2;
     
     band6.setBounds(band1.getX(), bottomMargin, faderWidth, faderheight);
+    band6Label.setFont(juce::Font ("Helvetica", band1.getWidth() * 0.2f, juce::Font::FontStyleFlags::bold));
+    
     band7.setBounds(band2.getX(), bottomMargin, faderWidth, faderheight);
+    band7Label.setFont(juce::Font ("Helvetica", band1.getWidth() * 0.2f, juce::Font::FontStyleFlags::bold));
+    
     band8.setBounds(band3.getX(), bottomMargin, faderWidth, faderheight);
+    band8Label.setFont(juce::Font ("Helvetica", band1.getWidth() * 0.2f, juce::Font::FontStyleFlags::bold));
+    
     band9.setBounds(band4.getX(), bottomMargin, faderWidth, faderheight);
+    band9Label.setFont(juce::Font ("Helvetica", band1.getWidth() * 0.2f, juce::Font::FontStyleFlags::bold));
+    
     band10.setBounds(band5.getX(), bottomMargin, faderWidth, faderheight);
+    band10Label.setFont(juce::Font ("Helvetica", band1.getWidth() * 0.2f, juce::Font::FontStyleFlags::bold));
 }
--- Multi-Q/Source/UI-Components/GraphicEQComponent.h
+++ Multi-Q/Source/UI-Components/GraphicEQComponent.h
@@ -27,15 +27,35 @@ public:
 private:
     
     viator_gui::Fader band1 {" dB", "31", -12.0, 12.0, 0.01, 0.0};
+    viator_gui::Label band1Label;
+    
     viator_gui::Fader band2 {" dB", "63", -12.0, 12.0, 0.01, 0.0};
+    viator_gui::Label band2Label;
+    
     viator_gui::Fader band3 {" dB", "125", -12.0, 12.0, 0.01, 0.0};
+    viator_gui::Label band3Label;
+    
     viator_gui::Fader band4 {" dB", "250", -12.0, 12.0, 0.01, 0.0};
+    viator_gui::Label band4Label;
+    
     viator_gui::Fader band5 {" dB", "500", -12.0, 12.0, 0.01, 0.0};
+    viator_gui::Label band5Label;
+    
     viator_gui::Fader band6 {" dB", "1K", -12.0, 12.0, 0.01, 0.0};
+    viator_gui::Label band6Label;
+    
     viator_gui::Fader band7 {" dB", "2K", -12.0, 12.0, 0.01, 0.0};
+    viator_gui::Label band7Label;
+    
     viator_gui::Fader band8 {" dB", "4K", -12.0, 12.0, 0.01, 0.0};
+    viator_gui::Label band8Label;
+    
     viator_gui::Fader band9 {" dB", "8K", -12.0, 12.0, 0.01, 0.0};
+    viator_gui::Label band9Label;
+    
     viator_gui::Fader band10 {" dB", "15K", -12.0, 12.0, 0.01, 0.0};
+    viator_gui::Label band10Label;
+    
     
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (GraphicEQComponent)
 };
