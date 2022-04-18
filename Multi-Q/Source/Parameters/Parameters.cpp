977083307e24220e9aa4e4506f968ca5a905f6f0
Landon <33662627+landonviator@users.noreply.github.com>
2022-04-17 20:52:19 -0500

    added stereo logic to hp and lp filters

--- Multi-Q/Source/Parameters/Parameters.cpp
+++ Multi-Q/Source/Parameters/Parameters.cpp
@@ -15,7 +15,33 @@ void MultiQAudioProcessor::updateCommonParameters()
     osToggle = static_cast<bool>(treeState.getRawParameterValue(qualityID)->load());
     phaseToggle = treeState.getRawParameterValue(phaseID)->load();
     DBG(static_cast<int>(phaseToggle));
+    
     hpFilter.setParameter(viator_dsp::SVFilter<float>::ParameterId::kCutoff, treeState.getRawParameterValue(highpassID)->load());
     lpFilter.setParameter(viator_dsp::SVFilter<float>::ParameterId::kCutoff, treeState.getRawParameterValue(lowpassID)->load());
+
+    switch (static_cast<int>(treeState.getRawParameterValue(msID)->load()))
+    {
+        case 0:
+        {
+            hpFilter.setStereoType(viator_dsp::SVFilter<float>::StereoId::kStereo);
+            lpFilter.setStereoType(viator_dsp::SVFilter<float>::StereoId::kStereo);
+            break;
+        }
+            
+        case 1:
+        {
+            hpFilter.setStereoType(viator_dsp::SVFilter<float>::StereoId::kMids);
+            lpFilter.setStereoType(viator_dsp::SVFilter<float>::StereoId::kMids);
+            break;
+        }
+            
+        case 2:
+        {
+            hpFilter.setStereoType(viator_dsp::SVFilter<float>::StereoId::kSides);
+            lpFilter.setStereoType(viator_dsp::SVFilter<float>::StereoId::kSides);
+            break;
+        }
+    }
+    
     gainModule.setGainDecibels(treeState.getRawParameterValue(trimID)->load());
 }
