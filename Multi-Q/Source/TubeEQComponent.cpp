c7b76c306419440d5fabbf4ca94ad411d59134bd
Landon <33662627+landonviator@users.noreply.github.com>
2022-04-17 19:52:01 -0500

    added resizing labels to tube eq component

--- Multi-Q/Source/TubeEQComponent.cpp
+++ Multi-Q/Source/TubeEQComponent.cpp
@@ -15,17 +15,50 @@
 TubeEQComponent::TubeEQComponent()
 {
     addAndMakeVisible(lowBoostDial);
+    addAndMakeVisible(lowBoostLabel);
+    lowBoostLabel.setText("Low Boost", juce::dontSendNotification);
+    lowBoostLabel.attachToComponent(&lowBoostDial, false);
+    lowBoostLabel.setJustificationType(juce::Justification::centred);
+    
     addAndMakeVisible(lowCutDial);
+    addAndMakeVisible(lowCutLabel);
+    lowCutLabel.setText("Low Cut", juce::dontSendNotification);
+    lowCutLabel.attachToComponent(&lowCutDial, false);
+    lowCutLabel.setJustificationType(juce::Justification::centred);
+    
     addAndMakeVisible(highBoostDial);
+    addAndMakeVisible(highBoostLabel);
+    highBoostLabel.setText("High Boost", juce::dontSendNotification);
+    highBoostLabel.attachToComponent(&highBoostDial, false);
+    highBoostLabel.setJustificationType(juce::Justification::centred);
+    
     addAndMakeVisible(highCutDial);
+    addAndMakeVisible(highCutLabel);
+    highCutLabel.setText("High Cut", juce::dontSendNotification);
+    highCutLabel.attachToComponent(&highCutDial, false);
+    highCutLabel.setJustificationType(juce::Justification::centred);
     
     addAndMakeVisible(lowFreqDial);
     lowFreqDial.setSkewFactorFromMidPoint(100.0);
+    addAndMakeVisible(lowFreqLabel);
+    lowFreqLabel.setText("Low Freq", juce::dontSendNotification);
+    lowFreqLabel.attachToComponent(&lowFreqDial, false);
+    lowFreqLabel.setJustificationType(juce::Justification::centred);
+    
     
     addAndMakeVisible(highFreqDial);
-    highFreqDial.setSkewFactorFromMidPoint(6000.0);
+    highFreqDial.setSkewFactorFromMidPoint(5000.0);
+    addAndMakeVisible(highFreqLabel);
+    highFreqLabel.setText("High Freq", juce::dontSendNotification);
+    highFreqLabel.attachToComponent(&highFreqDial, false);
+    highFreqLabel.setJustificationType(juce::Justification::centred);
+    
     
     addAndMakeVisible(bandwidthDial);
+    addAndMakeVisible(bandwidthLabel);
+    bandwidthLabel.setText("BW", juce::dontSendNotification);
+    bandwidthLabel.attachToComponent(&bandwidthDial, false);
+    bandwidthLabel.setJustificationType(juce::Justification::centred);
 }
 
 TubeEQComponent::~TubeEQComponent()
@@ -44,13 +77,26 @@ void TubeEQComponent::resized()
     auto dialSize = getHeight() * 0.4;
     auto smallDialSize = dialSize * 0.85;
     auto spaceBetweenDials = 1.05;
+    auto labelSize = dialSize * 0.08f;
     
     lowBoostDial.setBounds(leftMargin, topMargin, dialSize, dialSize);
+    lowBoostLabel.setFont(juce::Font ("Helvetica", labelSize, juce::Font::FontStyleFlags::bold));
+
     lowCutDial.setBounds(lowBoostDial.getX() + lowBoostDial.getWidth() * spaceBetweenDials, topMargin, dialSize, dialSize);
+    lowCutLabel.setFont(juce::Font ("Helvetica", labelSize, juce::Font::FontStyleFlags::bold));
+
     highBoostDial.setBounds(lowCutDial.getX() + lowCutDial.getWidth() * spaceBetweenDials, topMargin, dialSize, dialSize);
+    highBoostLabel.setFont(juce::Font ("Helvetica", labelSize, juce::Font::FontStyleFlags::bold));
+
     highCutDial.setBounds(highBoostDial.getX() + highBoostDial.getWidth() * spaceBetweenDials, topMargin, dialSize, dialSize);
+    highCutLabel.setFont(juce::Font ("Helvetica", labelSize, juce::Font::FontStyleFlags::bold));
         
     lowFreqDial.setBounds(lowBoostDial.getX() + (lowBoostDial.getWidth() / 1.55), lowBoostDial.getY() + lowBoostDial.getHeight() * 1.2, smallDialSize, smallDialSize);
+    lowFreqLabel.setFont(juce::Font ("Helvetica", labelSize, juce::Font::FontStyleFlags::bold));
+
     bandwidthDial.setBounds(lowFreqDial.getX() + lowFreqDial.getWidth() * 1.3, lowFreqDial.getY(), smallDialSize, smallDialSize);
+    bandwidthLabel.setFont(juce::Font ("Helvetica", labelSize, juce::Font::FontStyleFlags::bold));
+
     highFreqDial.setBounds(highBoostDial.getX() + (highBoostDial.getWidth() / 1.55), bandwidthDial.getY(), smallDialSize, smallDialSize);
+    highFreqLabel.setFont(juce::Font ("Helvetica", labelSize, juce::Font::FontStyleFlags::bold));
 }
--- Multi-Q/Source/TubeEQComponent.h
+++ Multi-Q/Source/TubeEQComponent.h
@@ -27,12 +27,25 @@ public:
 private:
     
     viator_gui::Dial lowBoostDial {" dB", "Low Boost", 0.0, 10.0, 0.1, 0.0};
+    viator_gui::Label lowBoostLabel;
+    
     viator_gui::Dial lowCutDial {" dB", "Low Cut", 0.0, 10.0, 0.1, 0.0};
+    viator_gui::Label lowCutLabel;
+    
     viator_gui::Dial lowFreqDial {" Hz", "Low Freq", 30.0, 300.0, 1.0, 30.0};
+    viator_gui::Label lowFreqLabel;
+    
     viator_gui::Dial bandwidthDial {" BW", "Band Width", 0.0, 10.0, 0.1, 0.0};
+    viator_gui::Label bandwidthLabel;
+    
     viator_gui::Dial highBoostDial {" dB", "High Boost", 0.0, 10.0, 0.1, 0.0};
+    viator_gui::Label highBoostLabel;
+    
     viator_gui::Dial highCutDial {" dB", "High Cut", 0.0, 10.0, 0.1, 0.0};
+    viator_gui::Label highCutLabel;
+    
     viator_gui::Dial highFreqDial {" Hz", "High Freq", 1000.0, 16000.0, 1.0, 0.0};
+    viator_gui::Label highFreqLabel;
     
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (TubeEQComponent)
 };
