0eec0738186ec305612459bc5a5b75c5cd806950
Landon <33662627+landonviator@users.noreply.github.com>
2022-04-19 22:38:22 -0500

    added logic to make screen size good on small and large displays

--- Multi-Q/Source/UI-Methods/UI-InitWindow.cpp
+++ Multi-Q/Source/UI-Methods/UI-InitWindow.cpp
@@ -18,13 +18,23 @@ void MultiQAudioProcessorEditor::initWindow()
     // Using the width is more useful than the height, because we know the height will always be < than width
     int x = r.getWidth();
     
-    // Plugin window will always initialize to half the screen width by half of that for a rectangle
-    auto width = x / 2.0;
-    auto height = width / 2.0;
+    auto width = 0;
+    
+    if (r.getWidth() <= 1440)
+    {
+        width = x;
+    }
+    
+    else
+    {
+        width = x * 0.5;
+    }
+    
+    auto height = width * 0.5;
     
     //Making the window resizable by aspect ratio and setting size
     AudioProcessorEditor::setResizable(true, true);
-    AudioProcessorEditor::setResizeLimits(width * 0.75, height * 0.75, width * 1.25, height * 1.25);
+    AudioProcessorEditor::setResizeLimits(width * 0.5, height * 0.5, width * 1.25, height * 1.25);
     AudioProcessorEditor::getConstrainer()->setFixedAspectRatio(2.0);
     
     if (audioProcessor.windowWidth != 0.0)
@@ -37,5 +47,5 @@ void MultiQAudioProcessorEditor::initWindow()
         setSize (width, height);
     }
     
-    constructorFinished = true;
+    constructorFinished = true;    
 }
